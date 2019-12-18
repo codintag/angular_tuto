9ceda6d51695570d8d8a49c9bdacf1386c58b1f7
SuSu <susu@MacBook-Pro-de-SuSu.local>
2019-12-18 06:27:46 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tutopenclass';
+  isAuth = false;
+
+  appareilOne = "Machine à laver";
+  appareilTwo = "Ordinateur";
+  appareilThree = "Machine à coudre";
+
+  constructor () {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 4000
+    );
+  }
+  onAllumer(){
+    console.log('On allume tout!');
+  }
+
 }
